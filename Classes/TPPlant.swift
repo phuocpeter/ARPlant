29e98cab5fcbcab45d26521f3ec7c47cf8e0fc9a
Phuoc <phuocpeter19@gmail.com>
2016-09-01 12:26:32 -0400

    Implement TPPlant class

---
+++ Classes/TPPlant.swift
@@ -0,0 +1,18 @@
+//
+//  TPPlant.swift
+//  ARPlant
+//
+//  Created by Tran Thai Phuoc on 2016-09-01.
+//  Copyright © 2016 Tran Thai Phuoc. All rights reserved.
+//
+
+import UIKit
+
+class TPPlant: NSObject {
+  
+  let latitude: Double = 0
+  let longitude: Double = 0
+  let currentEXP: Double = 0
+  let currentStage: Int = 0
+  
+}
